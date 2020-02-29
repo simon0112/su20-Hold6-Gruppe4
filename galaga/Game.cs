884306af5d385362e7da5a903cadfd65266b8ad1
Anton Makki <antonmakki@Antons-MacBook-Pro.local>
2020-02-29 14:49:11 +0100

    new version

--- galaga/Game.cs
+++ galaga/Game.cs
@@ -1,38 +1,57 @@
 using DIKUArcade;
 using DIKUArcade.Timers;
 using DIKUArcade.EventBus;
-// using DIKUArcade.Entities;
-// using DIKUArcade.Graphics;
-// using DIKUArcade.Math;
-// using System.IO;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.Entities;
+using System.IO;
 using System;
+using System.Collections.Generic;
+
 
 
 public class Game : IGameEventProcessor<object>
 {
-    // private Player player;
     private Window win;
+    private Player player;
     private DIKUArcade.Timers.GameTimer gameTimer;
-    public Game() {
+    private GameEventBus<object> eventBus;
+    public Game() 
+    {
         // TODO: Choose some reasonable values for the window and timer constructor.
         // For the window, we recommend a 500x500 resolution (a 1:1 aspect ratio).
         win = new Window("Main" , 500, 500);
         gameTimer = new GameTimer(60, 60);
 
-        // player = new Player(
-        //     new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
-        //     new Image(Path.Combine("Assets", "Images", "Player.png")));
+        player = new Player(
+         new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+         new Image(Path.Combine("Assets", "Images", "Player.png")));
+
+        eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType>() {
+                GameEventType.InputEvent, // key press / key release
+                GameEventType.WindowEvent, // messages to the window 
+                });
+        win.RegisterEventBus(eventBus);
+        eventBus.Subscribe(GameEventType.InputEvent, this);
+        eventBus.Subscribe(GameEventType.WindowEvent, this);
+
     }
-    public void GameLoop() {
+        public void GameLoop() {
         while(win.IsRunning()) {
             gameTimer.MeasureTime();
             while (gameTimer.ShouldUpdate()) {
                 win.PollEvents();
                 // Update game logic here
+                player.Move();
             }
+            
+
             if (gameTimer.ShouldRender()) {
                 win.Clear();
                 // Render gameplay entities here
+                eventBus.ProcessEvents();
+                player.Entity.RenderEntity();
                 win.SwapBuffers();
             }
             if (gameTimer.ShouldReset()) {
@@ -41,14 +60,53 @@ public class Game : IGameEventProcessor<object>
             }
         }
     }
-    public void KeyPress(string key) {
-        throw new NotImplementedException();
+    private void KeyPress(string key) {
+        switch(key) {
+            case "KEY_ESCAPE":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                    GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                break;
+            case "KEY_LEFT":
+                eventBus.RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                    GameEventType.InputEvent, this, "KEY_LEFT", "", ""));
+                    // rigrige syntakse for en vector som input
+                    player.Direction(new Vec2F(0.01f, 0.1f));
+            break;
+
+        /*
+            * TODO: Add cases that call player.Direction with a suitable direction.
+            * You can match on cases such as "KEY_UP", "KEY_1", "KEY_A", etc.
+            * Remember that the entire screen is 1.0f wide/tall, so choose a
+            * fittingly small number for the direction, e.g. (0.01f,0.0f).
+        */
+
+        }
     }
     public void KeyRelease(string key) {
-        throw new NotImplementedException();
+        player.Direction(new Vec2F(0.01f, 0.1f));
     }
     public void ProcessEvent(GameEventType eventType,
         GameEvent<object> gameEvent) {
-        throw new NotImplementedException();
-}
+        if (eventType == GameEventType.WindowEvent) {
+            switch (gameEvent.Message) {
+                case "CLOSE_WINDOW":
+                    win.CloseWindow();
+                    break;
+                default:
+                    break;
+            }
+        } else if (eventType == GameEventType.InputEvent) {
+            switch (gameEvent.Parameter1) {
+                case "KEY_PRESS":
+                   KeyPress(gameEvent.Message);
+                   break;
+                case "KEY_RELEASE":
+                    KeyRelease(gameEvent.Message);
+                    break;
+            }     
+        }
+    }
+
 }
--- galaga/Player.cs
+++ galaga/Player.cs
@@ -1,13 +1,28 @@
-// using DIKUArcade;
-// using DIKUArcade.EventBus;
-// using DIKUArcade.Graphics;
-// using System.IO;
-// using System;
+using DIKUArcade.Graphics;
+using DIKUArcade.Entities;
+using DIKUArcade.EventBus;
+using System;
+using DIKUArcade.Math;
 
+public class Player : IGameEventProcessor<object> 
+{
+    public Entity Entity {get; private set;}
+    public Player(DynamicShape shape, IBaseImage image) 
+    {
+        Entity = new Entity(shape, image);
+    }
+    public void ProcessEvent(GameEventType eventType,
+        GameEvent<object> gameEvent) {
+        throw new NotImplementedException();  
+    }
+    public void Direction(Vec2F dir)
+    {
+        Entity.Shape.AsDynamicShape().ChangeDirection(dir);
 
-// public class Player : IGameEventProcessor<object> {
-//     public Entity Entity {get; private set;}
-//     public Player(DynamicShape shape, IBaseImage image) {
-//         Entity = new Entity(shape, image);
-//     }
-// }
+    }
+    public void Move()
+    {
+        Entity.Shape.Move();
+    }
+}
+   
